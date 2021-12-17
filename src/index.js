3c585a3176298925978131ce5caf4ff4ff342b3c
unknown <christoph02.cw@gmail.com>
2021-12-17 20:47:54 +0100

    :construction: first ThreeJs code / remove main.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import * as THREE from "three";
-import "./main";
 
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({canvas});
+
+let fov = 75;
+let aspect = 2;  // the canvas default
+let near = 0.1;
+let far = 5;
+
+let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+renderer.render(scene, camera);
