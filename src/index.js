dc3e084dcc23a7329c7c9b4e621c2680054cfc33
unknown <christoph02.cw@gmail.com>
2021-12-17 20:48:59 +0100

    :construction: animated cube

--- src/index.js
+++ src/index.js
@@ -26,3 +26,15 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 renderer.render(scene, camera);
+
+function render(time) {
+    time *= 0.001;  // convert time to seconds
+   
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+   
+    renderer.render(scene, camera);
+   
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
