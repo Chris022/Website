074983ea138f93187c5078c39019dcfbdd4a53f0
unknown <christoph02.cw@gmail.com>
2021-12-18 20:19:34 +0100

    :sparkles: add clouds

--- src/objects/scene.js
+++ src/objects/scene.js
@@ -4,10 +4,10 @@ import ambiantLight         from "./ambientLight"
 import directionalLight     from "./directionalLight"
 import car                  from "./car"
 import earth                from "./earth"
+import clouds               from "./clouds";
 
 
-
-let drawableObjects = [car,earth]
+let drawableObjects = [car,earth].concat(clouds)
 let otherObjectes = [ambiantLight,directionalLight]
 
 let background = new THREE.Color( 0x77D6FF );
