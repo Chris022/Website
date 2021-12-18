0bb2d30629c339ebb425579568f7eeb44d6078bb
unknown <christoph02.cw@gmail.com>
2021-12-18 20:21:40 +0100

    :construction: change position and fov

--- src/objects/camera.js
+++ src/objects/camera.js
@@ -1,11 +1,11 @@
 import * as THREE from "three";
 
-let fov = 100;
+let fov = 50;
 let aspect = 2;  // the canvas default
-let near = 0.1;
-let far = 20;
+let near = 0.01;
+let far = 50;
 let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 15;
+camera.position.z = 30;
 camera.lookAt (new THREE.Vector3(0,0,0));
 
 export default camera;
