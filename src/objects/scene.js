01109616d361f23a3d79e3b46e16e24c3606ca5b
unknown <christoph02.cw@gmail.com>
2021-12-18 18:22:27 +0100

    :construction: add car and earth to scene

--- src/objects/scene.js
+++ src/objects/scene.js
@@ -2,13 +2,14 @@ import createSceneObject    from "./objectTemplates/scene"
 import ambiantLight         from "./ambientLight"
 import directionalLight     from "./directionalLight"
 import car                  from "./car"
+import earth                from "./earth"
 
 
 
-let drawableObjects = [car]
+let drawableObjects = [car,earth]
 let otherObjectes = [ambiantLight,directionalLight]
 
-let background = 0xddeaff;
+let background = 0xFFFFF;
 
 let scene = createSceneObject(drawableObjects,otherObjectes,[],background)
 
