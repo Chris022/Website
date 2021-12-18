a8b6c73b3eac3d9b56e213c07b28f7dc4093b3bb
unknown <christoph02.cw@gmail.com>
2021-12-18 18:21:47 +0100

    :construction: make the car rotate around center/earth

--- src/objects/car.js
+++ src/objects/car.js
@@ -1,12 +1,36 @@
 import createDrawableObject from "./objectTemplates/drawable"
 import {loadGLBObject}      from "./../utils/modelLoading"
+import {MathUtils} from 'three';
+import * as THREE from "three";
 
 let mash = await loadGLBObject("./../resources/models/car/car.glb")
 
+function rotateAboutPoint(obj, point, axis, theta){
+
+    obj.position.sub(point); // remove the offset
+    obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
+    obj.position.add(point); // re-add the offset
+
+    obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+}
+
 let renderFunction = (time) => {
-    mash.rotation.x = time;
-    mash.rotation.y = time;
+    //Create a matrix
+    var matrix = new THREE.Matrix4();
+    //Rotate the matrix
+    matrix.makeRotationX(-Math.PI / 1000);
+    car.mesh.rotation.x -= Math.PI / 1000
+    //rotate the object using the matrix
+    car.mesh.position.applyMatrix4(matrix);
 }
 
 let car = createDrawableObject(mash,renderFunction)
+
+car.mesh.position.z = 10
+
+car.mesh.rotation.x = MathUtils.degToRad(90);
+car.mesh.rotation.y = MathUtils.degToRad(90);
+car.mesh.rotation.z = MathUtils.degToRad(0);
+
+
 export default car;
