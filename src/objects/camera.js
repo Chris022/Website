bdccc562a37e5a72b4cccdfe8de55ae50822be97
unknown <christoph02.cw@gmail.com>
2021-12-18 18:20:46 +0100

    :construction: make the Cam look at 0,0,0

--- src/objects/camera.js
+++ src/objects/camera.js
@@ -1,18 +1,12 @@
 import * as THREE from "three";
 
-let fov = 75;
+let fov = 100;
 let aspect = 2;  // the canvas default
 let near = 0.1;
 let far = 20;
 let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 4;
-camera.position.y = 3;
-camera.position.x = 3;
-
-camera.rotation.x = -3.14/6;
-camera.rotation.y = 3.14/6;
-camera.rotation.z = 3.14/11;
-
+camera.position.z = 15;
+camera.lookAt (new THREE.Vector3(0,0,0));
 
 export default camera;
 
