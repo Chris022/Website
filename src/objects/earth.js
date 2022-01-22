db4c43cbb98eed9735b5f8be80308d390849523d
unknown <christoph02.cw@gmail.com>
2022-01-22 20:35:12 +0100

    :construction: make earth load model

--- src/objects/earth.js
+++ src/objects/earth.js
@@ -3,36 +3,12 @@ import createDrawableObject from "./objectTemplates/drawable"
 import {loadGLBObject}      from "./../utils/modelLoading"
 import { Mesh } from "three";
 
-const radius = 9.1;  // ui: radius
-const widthSegments = 100;  // ui: widthSegments
-const heightSegments = 100;  // ui: heightSegments
-const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+let mesh = await loadGLBObject("./../resources/models/ball.glb")
 
-let material = new THREE.MeshLambertMaterial({color: 0x1980CF});
-
-let sphere = new THREE.Mesh(geometry, material);
-
-let mesh = await loadGLBObject("./../resources/models/earth.glb")
-
-let rot = Math.PI/2;
-let quater = 0
-let renderFunction = (time,keyDict) => {
-    //if(keyDict["a"]){
-    //    rot += Math.PI/100
-    //}
-    //else if(keyDict["d"]){
-    //    rot -= Math.PI/100;
-    //}
-    //if(keyDict["w"]){
-    //    mash.rotation.x += Math.sin(rot)/200;
-    //    mash.rotation.y -= Math.cos(rot)/200;
-    //}
-
-}
+let renderFunction = (time,keyDict) => {}
 
 let earth = createDrawableObject(mesh,renderFunction); // = createDrawableObject(sphere,renderFunction)
 
 earth.mesh.rotation.x = Math.PI/2;
-
 
 export default earth;
