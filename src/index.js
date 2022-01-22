efa7a62c27d2cc14830dafd4ec5d24d7b4a1bd50
unknown <christoph02.cw@gmail.com>
2022-01-22 20:05:49 +0100

    :construction: remove mouse rotation controls

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import * as THREE from "three";
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+//import ui from "./ui"
 import camera from "./objects/camera";
 import scene from "./objects/scene";
 
 //Get the Canvas and set it as the display Canvas
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setPixelRatio( window.devicePixelRatio );
 
 //Set the renderer to use the camera and scene
 renderer.render(scene.scene, camera);
 
-let controls = new OrbitControls(camera, renderer.domElement);
- 
-controls.target.set(0, 0, 0);
-controls.enablePan = false;
-controls.maxPolarAngle = Math.PI / 2;
-controls.enableDamping = true;
+//let controls = new OrbitControls(camera, renderer.domElement);
+//
+//controls.target.set(0, 0, 0);
+//controls.enablePan = false;
+//controls.maxPolarAngle = Math.PI / 2;
+//controls.enableDamping = true;
 
 function render(time) {
     time *= 0.001;  // convert time to seconds
