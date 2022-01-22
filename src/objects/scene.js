6dbedfe88830d972e86103ae567960557e805a9f
unknown <christoph02.cw@gmail.com>
2022-01-22 20:40:25 +0100

    :construction: add button

--- src/objects/scene.js
+++ src/objects/scene.js
@@ -5,9 +5,10 @@ import directionalLight     from "./directionalLight"
 import car                  from "./car"
 import earth                from "./earth"
 import clouds               from "./clouds";
+import button               from "./button";
 
 
-let drawableObjects = [earth,car]//.concat(clouds)
+let drawableObjects = [earth,button,car]//.concat(clouds)
 let otherObjectes = [ambiantLight,directionalLight]
 
 let background = new THREE.Color( 0x77D6FF );
