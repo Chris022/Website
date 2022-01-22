77d4b2ca7bf65e269f13237ef89882598a17ce5d
unknown <christoph02.cw@gmail.com>
2022-01-22 20:03:00 +0100

    :construction: edit cam

--- src/objects/camera.js
+++ src/objects/camera.js
@@ -4,11 +4,26 @@ import {MathUtils} from 'three';
 let fov = 50;
 let aspect = 2;  // the canvas default
 let near = 0.01;
-let far = 50;
+let far = 70;
+
 let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.y = 30;
+
+
+export let setPositon = (vect) => {
+    camera.position.x = 45*vect.x
+    camera.position.y = 45*vect.y
+    camera.position.z = 45*vect.z
+}
+export let setRotation = (vect,rot) => {
+    camera.rotateOnWorldAxis(vect,rot)
+}
+camera.position.y = 45
+
+
 camera.lookAt(0,0,0)
-camera.rotation.z = MathUtils.degToRad(-90);
+
+camera.rotation.z = -Math.PI/2
+
 camera.updateMatrix();
 
 export default camera;
