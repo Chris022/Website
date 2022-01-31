2700e414aef588d0ddd060778f4c71cef9f77304
unknown <christoph02.cw@gmail.com>
2022-01-31 20:07:44 +0100

    :construction: change earth model

--- src/objects/earth.js
+++ src/objects/earth.js
@@ -5,6 +5,7 @@ import { Mesh } from "three";
 
 let mesh = await loadGLBObject("./../resources/models/ball.glb")
 
+
 let renderFunction = (time,keyDict) => {}
 
 let earth = createDrawableObject(mesh,renderFunction); // = createDrawableObject(sphere,renderFunction)
