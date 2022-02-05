984f6b39b30020eb3f27cfb34a38fc24b9dc933b
unknown <christoph02.cw@gmail.com>
2022-02-05 15:55:32 +0100

    :fire: remove moved files

--- src/objects/ambientLight.js
+++
@@ -1,4 +0,0 @@
-import * as THREE from "three";
-
-let ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
-export default ambientLight
--- src/objects/directionalLight.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from "three";
-
-let directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-directionalLight.position.set(0, 5, 5); // x, y, z
-
-export default directionalLight;
