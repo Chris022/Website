3b133cc81bfe4a51327d04124f0fc032244da989
unknown <christoph02.cw@gmail.com>
2022-02-07 18:25:41 +0100

    :fire: removed unused file

--- src/threeJS/objects/directionalLight.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-
-let directionalLight1 = new THREE.DirectionalLight(0xffffff, 1.5);
-directionalLight1.position.set(0, 30, 0); // x, y, z
-directionalLight1.castShadow = true;
-//directionalLight1.lookAt(new THREE.Vector3(0,0,0))
-
-
-export default directionalLight1;
