9cc435ac81a4b122d8a3f264ce97509aec584fb4
unknown <christoph02.cw@gmail.com>
2022-02-08 15:28:34 +0100

    :construction: change some text and text size

--- src/reactJS/AboutMe.js
+++ src/reactJS/AboutMe.js
@@ -79,19 +79,19 @@ export default function AboutMe({openModalTigger}) {
                     <Grid item xs>
                         <div style={{ "marginLeft": "2%" }}>
                             <Typography variant="h6" style={{ "fontFamily": "MinSans" }}>Introduction:</Typography>
-                            <Typography variant="p" style={{ "fontFamily": "MinSans" }}>
+                            <Typography variant="p" style={{ "fontFamily": "MinSans","fontSize":18}}>
                                 Hi, my name is Christoph. I am 19 Years old and I am in my senior year at the HTL-Salzburg.
                             </Typography>
                             <br />
                             <br />
                             <Typography variant="h6" style={{ "fontFamily": "MinSans" }}>Hobbies:</Typography>
-                            <Typography variant="p" style={{ "fontFamily": "MinSans" }}>
+                            <Typography variant="p" style={{ "fontFamily": "MinSans","fontSize":18}}>
                                 Rock-Climbing🧗, Cycling🚴, Running🏃, Programming and pretty much everything else you can do on a computer💻
                             </Typography>
                             <br />
                             <br />
                             <Typography variant="h6" style={{ "fontFamily": "MinSans" }}>My Jobs:</Typography>
-                            <Typography variant="p" style={{ "fontFamily": "MinSans" }}>
+                            <Typography variant="p" style={{ "fontFamily": "MinSans","fontSize":18}}>
                                 <ul>
                                     <li>3 x Internships at a small Software Company / Mainly Web-Programming, C#, Networking</li>
                                     <li>1 Year of work at the same Company (8 Hours a Week, when not at school)</li>
--- src/reactJS/Greeting.js
+++ src/reactJS/Greeting.js
@@ -16,7 +16,7 @@ export default function Greeting() {
         <Dialog
             open={open}
             fullWidth
-            maxWidth="xs"
+            maxWidth="lg"
             PaperProps={{
                 style: {
                   //background: "linear-gradient( 45deg, #CA4246 16.666%,  #E16541 16.666%,  #E16541 33.333%,  #F18F43 33.333%, #F18F43 50%,  #8B9862 50%,  #8B9862 66.666%,  #476098 66.666%,  #476098 83.333%,  #A7489B 83.333%)",
@@ -27,13 +27,17 @@ export default function Greeting() {
         >
             <DialogTitle><center><Typography variant="h2" style={{"fontFamily":"MinSans"}}>Hi!</Typography></center></DialogTitle>
             <div style={{"margin":"10px"}}>
-                <Typography style={{"fontFamily":"MinSans"}}>
+                <Typography style={{"fontFamily":"MinSans-Normal","fontSize":23}}>
                     I am Chris and this is my little World. Feel free to explore it.
                     Have Fun!
                 </Typography>
-                <Typography style={{"fontFamily":"MinSans"}}>
-                    Use the arrow keys to steer and rotate the world using the left button
-                    on your mouse. If you tent to get seesick, you can also  just use the drawer in the top right.
+                <Typography style={{"fontFamily":"MinSans-Normal","fontSize":23}}>
+                    Use the <b>W/S & A/D</b> keys to steer. Rotate the world using the left mouse button.
+                    In the top right you can see which country you are <b>looking at</b>.
+                    The red exclamation marks represent different ports! What are you waiting for? Go and explore them😊
+                </Typography>
+                <Typography style={{"fontFamily":"MinSans-Normal","fontSize":23}}>
+                    PS: If you tent to get seesick, you can also  just use the drawer in the top left.
                 </Typography>
             </div>
             <div style={{"margin":"10px"}}>
